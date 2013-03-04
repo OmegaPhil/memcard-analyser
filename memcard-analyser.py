c162e0a0ca70fe15f041216ef2d609c14c85a7f2
Omega Weapon <OmegaPhil@gmail.com>
2013-03-04 20:12:03 +0000

    Initial commit

---
+++ memcard-analyser.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+'''
+Version 0.01 2013.03.04
+Copyright (c) 2013, OmegaPhil - OmegaPhil+memcard-analyser@gmail.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+'''
+
+# Preparing for Python 3
+from __future__ import division, print_function, unicode_literals
+
+GPL_NOTICE = '''
+Copyright (C) 2013 OmegaPhil
+License GPLv3: GNU GPL version 3 <http://gnu.org/licenses/gpl.html>.
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+'''
+
